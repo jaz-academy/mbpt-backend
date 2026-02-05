bce2452bc265ede6df3f7b67000ecd5df7f62ee9
abukafa <hikam.abukafa@gmail.com>
2026-02-05 22:56:44 +0700

    fixed fixed

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://mbpt.jazacademy.id/'],
+    origin: ['http://localhost:5173', 'https://mbpt.jazacademy.id'],
     credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
